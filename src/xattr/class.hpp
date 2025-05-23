da0758f411d7a49ffb96c8b91abf4c3419553a57
Lumber4ack <protsyukvitalik@gmail.com>
2025-05-23 17:34:34 +0200

    no mac support

--- src/xattr/class.hpp
+++ src/xattr/class.hpp
@@ -11,16 +11,10 @@
 class CustomXAttr
 {
 public:
-    #ifdef _SYS_XATTR_H
     static int setcxa(const char *path, std::string value, int flags);
     static int getcxa(const char *path, std::string &value);
     //int removecxa(const char *path, const char *name);
     //ssize_t listcxa(const char *path, char *list, size_t size);
-    #endif
-    #ifdef _SYS_XATTR_H_
-    static int setcxa(const char *path, std::string value, int pos,int flags);
-    static int getcxa(const char *path, std::string &value, int pos, int flags);
-    #endif
 };
 
 #endif
