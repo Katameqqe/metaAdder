36cc8f5bef9c75eececbe210666fd8440400ef54
Lumber4ack <protsyukvitalik@gmail.com>
2025-05-23 17:21:02 +0200

    added macos support ?

--- src/xattr/class.cpp
+++ src/xattr/class.cpp
@@ -1,5 +1,6 @@
 #include "class.hpp"
 
+#ifdef _SYS_XATTR_H
 int CustomXAttr::setcxa(const char *path, std::string value, int flags)
 {
     return setxattr(path, _CXA_USER_PREFIX, value.c_str(), value.size(), flags);
@@ -15,3 +16,21 @@ int CustomXAttr::getcxa(const char *path, std::string &value)
     }
     return ret;
 }
+#endif
+
+#ifdef _SYS_XATTR_H_
+int CustomXAttr::setcxa(const char *path, std::string value, int pos, int flags)
+{
+    return setxattr(path, _CXA_USER_PREFIX, value.c_str(), value.size(), pos, flags);
+}
+int CustomXAttr::getcxa(const char *path, std::string &value, int pos, int flags)
+{
+    value.resize(256);
+    ssize_t ret = getxattr(path, _CXA_USER_PREFIX, &value[0], value.size(), pos, flags);
+    if (-1 != ret)
+    {
+        value.resize(ret);
+    }
+    return ret;
+}
+#endif
