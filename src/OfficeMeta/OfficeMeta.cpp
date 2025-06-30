e3f8898c681b5f15582624b04a3d1d1cc691ca9d
Lumber4ack <protsyukvitalik@gmail.com>
2025-06-30 21:06:40 +0200

    refoactored OfficeMeta + added rmProp func

--- src/OfficeMeta/OfficeMeta.cpp
+++ src/OfficeMeta/OfficeMeta.cpp
@@ -8,23 +8,17 @@ int OfficeMeta::setProp(const std::string &filePath, const std::string &name, co
     {
         return err;
     }
-    std::string cont_content;
-    std::string custom_content;
+
     std::string content;
+    std::size_t indx;
     if (zip_name_locate(za, "docProps/custom.xml", 0) == -1)
     {
-        zip_file_t *zf;
+        writeToStr(za, "[Content_Types].xml", content);
+        
         zip_source_t *zs;
-        zf = zip_fopen(za, "[Content_Types].xml",0);
-        std::vector<char> buff(4096);
-        zip_int64_t n;
-        while ((n = zip_fread(zf, buff.data(), buff.size())) > 0) {
-            cont_content.append(buff.data(), n);
-        }
-        zip_int64_t pos = cont_content.rfind("</Types>");
-        cont_content.insert(pos,"<Override PartName=\"/docProps/custom.xml\" ContentType=\"application/vnd.openxmlformats-officedocument.custom-properties+xml\"/>");
-        zip_fclose(zf);
-        zs = zip_source_buffer(za, cont_content.c_str(), cont_content.length(), 0);
+        zip_int64_t indx = content.rfind("</Types>");
+        content.insert(indx,"<Override PartName=\"/docProps/custom.xml\" ContentType=\"application/vnd.openxmlformats-officedocument.custom-properties+xml\"/>");
+        zs = zip_source_buffer(za, content.c_str(), content.length(), 0);
 
         if (!zs)
         {
@@ -40,14 +34,12 @@ int OfficeMeta::setProp(const std::string &filePath, const std::string &name, co
             zip_close(za);
             return -1;
         }
-        printf("%s\n",cont_content.c_str());
-
-        custom_content = std::string("<Properties xmlns=\"http://schemas.openxmlformats.org/officeDocument/2006/custom-properties\" xmlns:vt=\"http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes\">\r\n<property fmtid=\"{D5CDD505-2E9C-101B-9397-08002B2CF9AE}\" pid=\"2\" name=\"") + name + std::string("\"><vt:lpwstr>") + value + std::string("</vt:lpwstr></property></Properties>");
-        //printf("%s\n", custom_content.c_str());
-        zs = zip_source_buffer(za, custom_content.c_str(), custom_content.size(), 0);
+        content.clear();
+        content = "<Properties xmlns=\"http://schemas.openxmlformats.org/officeDocument/2006/custom-properties\" xmlns:vt=\"http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes\">\r\n<property fmtid=\"{D5CDD505-2E9C-101B-9397-08002B2CF9AE}\" pid=\"2\" name=\"" + name + "\"><vt:lpwstr>" + value + "</vt:lpwstr></property></Properties>";
+        zs = zip_source_buffer(za, content.c_str(), content.size(), 0);
         if (!zs)
         {
-            printf("Zcustom error");
+            printf("Zsource error");
             zip_close(za);
             return -1;
         }
@@ -61,16 +53,9 @@ int OfficeMeta::setProp(const std::string &filePath, const std::string &name, co
     }
     else
     {
-        zip_file_t *zf = zip_fopen(za, "docProps/custom.xml", 0);
-        std::vector<char> buf(4096);
+        writeToStr(za, "docProps/custom.xml", content);
 
-        zip_int64_t n;
-        while ((n = zip_fread(zf, buf.data(), buf.size())) > 0) {
-            content.append(buf.data(), n);
-        }
-        zip_fclose(zf);
-
-        std::string fstr = std::string("name=\"")+name+std::string("\">");
+        const std::string fstr = "name=\"" + name + "\">";
         const std::string fvp = "<vt:lpwstr>";
         const std::string lvp = "</vt:lpwstr>";
 
@@ -82,7 +67,7 @@ int OfficeMeta::setProp(const std::string &filePath, const std::string &name, co
             int size_n = content.find("\"",indx) - indx;
             int next_n = std::stoi(content.substr(indx, size_n))+1;
 
-            std::string result = std::string("<property fmtid=\"{D5CDD505-2E9C-101B-9397-08002B2CF9AE}\" pid=\"")+std::to_string(next_n)+std::string("\" name=\"")+name+std::string("\"><vt:lpwstr>")+value+std::string("</vt:lpwstr></property>");
+            std::string result = "<property fmtid=\"{D5CDD505-2E9C-101B-9397-08002B2CF9AE}\" pid=\""+std::to_string(next_n)+"\" name=\""+name+"\"><vt:lpwstr>"+value+"</vt:lpwstr></property>";
             indx = content.rfind("</Properties>");
             content.insert(indx, result);
         }
@@ -95,11 +80,21 @@ int OfficeMeta::setProp(const std::string &filePath, const std::string &name, co
         }
 
         zip_source_t *zs = zip_source_buffer(za, content.c_str(), content.size(), 0);
-        zip_file_add(za, "docProps/custom.xml", zs, ZIP_FL_OVERWRITE | ZIP_FL_ENC_UTF_8);
-
+        if (!zs)
+        {
+            printf("Zsource error");
+            zip_close(za);
+            return -1;
+        }
+        if (zip_file_add(za, "docProps/custom.xml", zs, ZIP_FL_OVERWRITE | ZIP_FL_ENC_UTF_8) < 0)
+        {
+            zip_source_free(zs);
+            printf("Zcustom add error");
+            zip_close(za);
+            return -1;
+        }
         zip_source_close(zs);
     }
-
     
     zip_close(za);
     
@@ -113,35 +108,88 @@ int OfficeMeta::getProp(const std::string &filePath, const std::string &name, st
     {
         return err;
     }
-    else 
+
+
+    if (zip_name_locate(za, "docProps/custom.xml", 0) == -1)
     {
-        if (zip_name_locate(za, "docProps/custom.xml", 0) == -1) return -1;
-        zip_file_t *zf = zip_fopen(za, "docProps/custom.xml", 0);
-        
-        std::vector<char> buf(4096);
-        std::string content;
-        zip_int64_t n;
-        while ((n = zip_fread(zf, buf.data(), buf.size())) > 0) {
-            content.append(buf.data(), n);
-        }
-        zip_fclose(zf);
-        std::string fstr = std::string("name=\"")+name+std::string("\">");
-        const std::string fvp = "<vt:lpwstr>";
-        const std::string lvp = "</vt:lpwstr>";
-        std::size_t indx = content.rfind(fstr.c_str());
-        if (indx != std::string::npos)
-        {
-            indx += fstr.size() + fvp.size();
-            std::size_t n = content.find(lvp.c_str(),indx) - indx;
-            value = content.substr(indx, n);
-        }
-    };
+        zip_close(za);
+        return -1;
+    }
+    
+    std::string content;
+    writeToStr(za, "docProps/custom.xml", content);
+
+    const std::string fstr = "name=\""+name+"\">";
+    const std::string fvp = "<vt:lpwstr>";
+    const std::string lvp = "</vt:lpwstr>";
+    std::size_t indx = content.rfind(fstr.c_str());
+    if (indx != std::string::npos)
+    {
+        indx += fstr.size() + fvp.size();
+        std::size_t n = content.find(lvp.c_str(),indx) - indx;
+        value = content.substr(indx, n);
+    }
     zip_close(za);
 
     return 0;
 }
 int OfficeMeta::rmProp(const std::string &filePath, const std::string &name)
 {
+    int err = 0;
+    zip_t *za = zip_open(filePath.c_str(), 0, &err);
+    if (!za)
+    {
+        return err;
+    }
+
+    if (zip_name_locate(za, "docProps/custom.xml", 0) == -1)
+    {
+        zip_close(za);
+        return -1;
+    }
+
+    std::string content;
+    writeToStr(za, "docProps/custom.xml", content);
+
+    const std::string fstr = "name=\""+name+"\">";
+    std::size_t indx = content.rfind(fstr.c_str());
+    if (indx != std::string::npos)
+    {
+        indx = content.rfind("<property ", indx);
+        std::size_t n = (content.find("</property>", indx) + 11) - indx;
+        content.erase(indx, n);
+    }
+    
+    zip_source_t *zs = zip_source_buffer(za, content.c_str(), content.size(), 0);
+    if (!zs)
+    {
+        printf("Zsource error");
+        zip_close(za);
+        return -1;
+    }
+    if (zip_file_add(za, "docProps/custom.xml", zs, ZIP_FL_OVERWRITE | ZIP_FL_ENC_UTF_8) < 0)
+    {
+        zip_source_free(zs);
+        printf("Zcustom add error");
+        zip_close(za);
+        return -1;
+    }
+    zip_source_close(zs);
+
+    zip_close(za);
+    
     return 0;
 
+}
+
+
+void OfficeMeta::writeToStr(zip_t *za, const std::string &fileName, std::string &buffer)
+{
+    zip_file_t *zf = zip_fopen(za, fileName.c_str(),0);
+    std::vector<char> buff(4096);
+    zip_int64_t n;
+    while ((n = zip_fread(zf, buff.data(), buff.size())) > 0) {
+        buffer.append(buff.data(), n);
+    }
+    zip_fclose(zf);
 }
--- src/OfficeMeta/OfficeMeta.hpp
+++ src/OfficeMeta/OfficeMeta.hpp
@@ -10,4 +10,6 @@ public:
     static int setProp(const std::string &filePath, const std::string &name, const std::string &value);
     static int getProp(const std::string &filePath, const std::string &name, std::string &value);
     static int rmProp(const std::string &filePath, const std::string &name);
+private:
+    static void writeToStr(zip_t *za, const std::string &fileName, std::string &buffer);
 };
